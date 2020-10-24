da85bb93a0b9665e0c8c447f86c7409987b6bf0f
Ree02 <ri.02kh.aax@gmail.com>
2020-10-24 18:01:42 +0900

    詳細のNULL値許可とコメント追加

--- database/migrations/2020_09_05_061134_create_reports_table.php
+++ database/migrations/2020_09_05_061134_create_reports_table.php
@@ -6,18 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateReportsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    // テーブルを生成する処理の記述
     public function up()
     {
+        // reportsテーブルの生成
         Schema::create('reports', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->foreignId('subject_id')->constrained('subjects');  // 外部キーを設定する
             $table->string('title', 20);
-            $table->string('detail', 100);
+            $table->string('detail', 100)->nullable(); // NULLを許可
             $table->date('due_date');
             $table->integer('status')->default(1);
             $table->softDeletes();
@@ -25,13 +22,10 @@ class CreateReportsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    // テーブルを削除するときの処理を記述
     public function down()
     {
+        // テーブルがあれば削除
         Schema::dropIfExists('reports');
     }
 }
