bf7b49f029ab787ed92c782bb19d180eec054f19
Ree02 <ri.02kh.aax@gmail.com>
2020-10-28 16:07:07 +0900

    科目編集ページ遷移のためのアイコン追加

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -3,25 +3,41 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col col-md-4">
-        <nav class="panel panel-default">
-          <div class="panel-heading">科目
-            <a href="{{ route('subjects.create') }}">
-               <span class="glyphicon glyphicon-plus color" aria-hidden="true"></span>
-            </a>
-          </div>
-          <div class="list-group">
-            @foreach($subjects as $subject)
-              <a 
-              href="{{ route('reports.index', ['id' => $subject->id]) }}" 
-              class="list-group-item {{ $current_subject_id === $subject->id ? 'active' : ''}}" 
-              >
-                {{ $subject->title }}
-              </a>
-            @endforeach
-          </div>
-        </nav>
+    <div class="column col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">科目
+          <a href="{{ route('subjects.create') }}">
+            <span class="glyphicon glyphicon-plus color" aria-hidden="true"></span>
+          </a>
+        </div>
+          <div class="panel-body">
+            <div class="text-right">
+            </div>
+
+          <table class="table">
+            <tbody>
+              @foreach($subjects as $subject)
+                <tr>
+                  <td class = "subject">
+                    <a 
+                      href="{{ route('reports.index', ['id' => $subject->id]) }}" 
+                      class="list-group-item {{ $current_subject_id === $subject->id ? 'active' : ''}}" style="border: none"
+                    >
+                      {{ $subject->title }}
+                    </a>
+                  </td>
+                  <td class = "subject">
+                    <a href="{{ route('reports.index', ['id' => $subject->id]) }}">
+                      <span class="glyphicon glyphicon-pencil color-edit" aria-hidden="true"></span>
+                    </a>
+                  </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
+    </div>
       <div class="column col-md-8">
       <div class="panel panel-default">
         <div class="panel-heading">課題
