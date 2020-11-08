f084f8d2e1d94ed2608d6d88e0b6350b065338e5
Ree02 <ri.02kh.aax@gmail.com>
2020-11-08 23:11:59 +0900

    subjectsテーブルにuser_id追加

---
+++ database/migrations/2020_11_08_222605_add_user_id_to_subjects.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToSubjects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->biginteger('user_id')->unsigned();
+
+            // 外部キーを設定する
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
