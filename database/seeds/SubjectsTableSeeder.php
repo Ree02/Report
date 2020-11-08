3ae31f0a457007138b8ca141ec26264a3364b94b
Ree02 <ri.02kh.aax@gmail.com>
2020-11-08 23:23:19 +0900

    シーダーの変更，追加

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -13,11 +13,13 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = DB::table('users')->first();
         $titles = ['数学', '英語', '物理'];
         
         foreach ($titles as $title){
             DB::table('subjects')->insert([
                 'title' => $title,
+                'user_id' => $user->id,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
