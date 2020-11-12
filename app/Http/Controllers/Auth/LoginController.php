9f01b91f7dda18f9dac86d52e8bfff11cf17db56
Ree02 <ri.02kh.aax@gmail.com>
2020-11-12 22:26:42 +0900

    ユーザIDに紐づいた科目を取得

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/subject/0/reports';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
