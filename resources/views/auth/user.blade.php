95f1c7132d5f39f3ab21ef88ca7ec2a7933ded72
Ree02 <ri.02kh.aax@gmail.com>
2020-11-29 15:09:39 +0900

    ユーザページにプロフィール画像表示

--- resources/views/auth/user.blade.php
+++ resources/views/auth/user.blade.php
@@ -3,18 +3,23 @@
 @section('content')
   <div class="container">
     <div class="panel panel-default">
-      <div class="panel-heading">ユーザ</div>
+      <div class="panel-heading">ユーザ情報</div>
       <div class="panel-body">
         <form action="{{ route('users') }}" method="post">
           @csrf
-          <table class="table">
-            <tbody>
-                <tr>
-                    <td>ユーザ名 : </td><td>{{ Auth::user()->name }}</td></tr>
-                    <tr><td>メールアドレス : </td><td>{{ Auth::user()->email }}</td>
-                </tr>
-            </tbody>
-          </table>
+          <div class="row">
+            <div class="col-sm-5">
+              <img src="{{ asset('storage/profiles/'. Auth::user()->profile_image) }}" alt="プロフィール画像">
+            </div>
+            <div class="col-sm-7">
+              <table class="table">
+                <tbody>
+                  <tr><td>ユーザ名 : </td><td>{{ Auth::user()->name }}</td></tr>
+                  <tr><td>メールアドレス : </td><td>{{ Auth::user()->email }}</td></tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
           <div class="text-right">
             <button type="submit" class="btn btn-ok" name="delete">アカウント削除</button>
           </div>
