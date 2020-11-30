2fdf148d8d13fce3a1302038c4448cc5c7fb4112
Ree02 <ri.02kh.aax@gmail.com>
2020-11-30 21:16:37 +0900

    ユーザ編集ページ遷移するボタン追加

--- resources/views/auth/user.blade.php
+++ resources/views/auth/user.blade.php
@@ -3,10 +3,8 @@
 @section('content')
   <div class="container">
     <div class="panel panel-default">
-      <div class="panel-heading">ユーザ情報</div>
+      <div class="panel-heading">ユーザー情報</div>
       <div class="panel-body">
-        <form action="{{ route('users') }}" method="post">
-          @csrf
           <div class="row">
             <div class="col-sm-5">
               <img src="{{ asset('storage/profiles/'. Auth::user()->profile_image) }}" alt="プロフィール画像">
@@ -21,9 +19,10 @@
             </div>
           </div>
           <div class="text-right">
-            <button type="submit" class="btn btn-ok" name="delete">アカウント削除</button>
+            <a href="{{ route('users.edit') }}">
+              <button type="submit" class="btn btn-ok" name="edit">ユーザー情報変更</button>
+            </a>
           </div>
-        </form>
       </div>
     </div>
   </div>
